d2c457b44145ef9cad45a2066173bcec507b66d2
AyrtonGonsallo <ayrtongonsallo444@gmail.com>
2021-12-22 18:35:25 +0100

    signup

--- signup.php
+++ signup.php
@@ -1,7 +1,7 @@
 <?php
 require "DataBase.php";
 $db = new DataBase();
-if (isset($_POST['firstname']) &&($_POST['lastname'])&& isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password'])) {
+if (isset($_POST['firstname']) &&($_POST['lastname'])&& isset($_POST['email']) && isset($_POST['DOB']) && isset($_POST['password'])) {
     if ($db->dbConnect()) {
         //function signUp($table, $firstname,$lastname, $email,$DOB,$phone, $password)
         if ($db->signUp("user1", $_POST['firstname'],$_POST['lastname'], $_POST['email'], $_POST['DOB'],$_POST['phone'], $_POST['password'])) {
